007877e38d87af9f4d423bfa095a47d24597262d
maschull106 <matthias.schuller@epfl.ch>
2023-04-26 19:55:20 +0200

    cleanup

--- hd_ws/build/manager/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/xplore/Desktop/main_HD_workspace/hd_ws/install/kerby_interfaces/share/kerby_interfaces/package.sh"
--- hd_ws/build/task_execution/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/xplore/Desktop/main_HD_workspace/hd_ws/install/task_execution'
