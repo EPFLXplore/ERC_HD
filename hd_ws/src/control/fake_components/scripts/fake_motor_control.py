6bf27087f12a5c3261e75f3a3062e0b7cd9913b2
maschull106 <matthias.schuller@ulb.ac.be>
2023-05-14 23:33:00 +0200

    transfered test scripts in fake_components package (untested)

--- hd_ws/src/control/fake_components/scripts/fake_motor_control.py
+++ hd_ws/src/control/fake_components/scripts/fake_motor_control.py
@@ -9,9 +9,6 @@ import copy
 import time
 
 
-MOTOR_COUNT = 6
-
-
 class Timer:
     def __init__(self):
         self.last_tick = time.time()
@@ -25,6 +22,9 @@ class Timer:
 
 
 class FakeMotorControl(Node):
+    MOTOR_COUNT = 6     # number of motors
+    MAX_VEL = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0]    # max velocity of each motor in rad/s
+
     def __init__(self):
         super().__init__("fake_motor_control")
         self.state = JointState()
@@ -36,16 +36,15 @@ class FakeMotorControl(Node):
         self.mimic = False   # wether to transmit commands from moveit directly to state (perfect motor)
 
         self.direct_manual_mode = True
-        self.cmd_velocities = [0.0]*MOTOR_COUNT
+        self.cmd_velocities = [0.0]*self.MOTOR_COUNT
         #self.cmd_velocities[0] = 1.0
         #self.cmd_velocities[2] = 1.0
         self.timer = Timer()
 
-
     def init_state(self):
-        self.state.position = [0.0]*MOTOR_COUNT
-        self.state.velocity = [0.0]*MOTOR_COUNT
-        self.state.effort = [0.0]*MOTOR_COUNT
+        self.state.position = [0.0]*self.MOTOR_COUNT
+        self.state.velocity = [0.0]*self.MOTOR_COUNT
+        self.state.effort = [0.0]*self.MOTOR_COUNT
 
     def moveit_cmd_callback(self, msg: JointState):
         if self.mimic:
@@ -54,7 +53,8 @@ class FakeMotorControl(Node):
             self.state.effort[:len(msg.effort)] = msg.effort
 
     def vel_cmd_callback(self, msg: Float64MultiArray):
-        self.state.cmd_velocities = msg.data
+        data = msg.data + [0.0]*max(0, self.MOTOR_COUNT-len(msg.data))
+        self.state.cmd_velocities = [max_ * vel for max_, vel in zip(self.MAX_VEL, data)]
 
     def publish_state(self):
         msg = copy.deepcopy(self.state)
@@ -64,7 +64,6 @@ class FakeMotorControl(Node):
         self.state.velocity = self.cmd_velocities
         t = self.timer.tick()
         self.state.position = [pos + vel*t for pos, vel in zip(self.state.position, self.cmd_velocities)]
-        print(self.state)
 
     def loop(self):
         rate = self.create_rate(25)
