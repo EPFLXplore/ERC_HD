e83a047de213aa34a5401749dbf02dc3243fd2be
Matthieu <matthieu.andre@epfl.ch>
2024-03-02 20:43:32 +0100

    single launch file baby !

--- hd_ws/src/vision/vision/controlpanel/ethernet_plug.py
+++
@@ -1,6 +0,0 @@
-from vision.controlpanel.plug import Plug
-
-
-class Ethernet_plug(Plug):
-    def __init__(self, top_left_corner, height, width) -> None:
-        super().__init__(top_left_corner, height, width)
