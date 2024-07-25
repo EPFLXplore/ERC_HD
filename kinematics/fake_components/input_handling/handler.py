0066a9fd362547052b3d305634a97741d09dd791
maschull106 <matthias.schuller@epfl.ch>
2024-07-25 16:02:45 +0200

    input handling made into a separate python package within fake_components + bugs corrected + tested

--- kinematics/fake_components/input_handling/handler.py
+++ kinematics/fake_components/input_handling/handler.py
@@ -10,16 +10,6 @@ import copy
 from dataclasses import dataclass
 from collections.abc import Callable
 from typing import List, Dict, Tuple, Any, Union
-import keyboard
-import keyboard._keyboard_event
-keyboard._os_keyboard.init()
-
-OS_KEYBOARD_INPUTS = copy.deepcopy(keyboard._os_keyboard.from_name)
-OS_KEYBOARD_INPUTS: Dict[str, List[Tuple[int, Tuple[str, ...]]]]
-for inp in ("A", "D", "M", "N", "O", "P"): OS_KEYBOARD_INPUTS[inp] = []
-
-
-InputType = Union[int, str]
 
 
 @dataclass
@@ -83,8 +73,8 @@ class EventHandlerConfig(metaclass=MetaEventHandler):
             Default ids are given by EventHandlerConfig.DEFAULT_IDS, default offsets are set to 0,  default amplitudes are set to 1.
         """
         self.input_ids : Dict[int, int] = {}
-        self.input_offsets : Dict[int, float] = {}
-        self.input_amplitudes : Dict[int, float] = {}
+        self.input_offsets : Dict[int, float] = {self.NULL: 0.0}
+        self.input_amplitudes : Dict[int, float] = {self.NULL: 1.0}
         for input, name in self.INPUTS.items():
             attr = f"{name}_id"
             self.input_ids[input] = kwargs.get(attr, self.DEFAULT_IDS[input])
@@ -191,8 +181,8 @@ class EventHandlerConfig(metaclass=MetaEventHandler):
         while True:
             self.read_inputs()
 
-    def background_loop(self):
-        thread = threading.Thread(target=self.loop, daemon=True)
+    def background_loop(self, daemon: bool = True):
+        thread = threading.Thread(target=self.loop, daemon=daemon)
         thread.start()
     
     def connect_events(src_handler: EventHandlerConfig, src_input: EventHandlerConfig.InputType, target_handler: EventHandlerConfig, target_input: EventHandlerConfig.InputType):
@@ -210,171 +200,3 @@ class EventHandlerConfig(metaclass=MetaEventHandler):
             target_handler.emit_signal(event_info)
         
         src_handler.bind(src_input, connection, "value")
-
-
-class _KeyboardConfig(EventHandlerConfig):
-    @classmethod
-    def _set_inputs_as_class_attributes(cls):
-        cls.INPUTS = {}
-        for ind, key in enumerate(OS_KEYBOARD_INPUTS):
-            if key.isalpha():
-                name = key
-            elif key.isnumeric():
-                name = "_" + key
-            else:
-                continue
-            setattr(cls, name, ind)
-            cls.INPUTS[ind] = key
-
-
-_KeyboardConfig._set_inputs_as_class_attributes()
-
-
-class KeyboardConfig(_KeyboardConfig):
-    """
-    Handler for keyboard
-    """
-    
-    DEFAULT_IDS = {inp: keyboard._canonical_names.normalize_name(key) for inp, key in _KeyboardConfig.INPUTS.items()}
-    
-    def __init__(self, **kwargs):
-        keyboard.on_press(self.handle_event)
-        super().__init__(**kwargs)
-        
-    def valid_event(self, event: keyboard._keyboard_event.KeyboardEvent) -> bool:
-        """
-        Overrides parent method
-        """
-        return True
-    
-    def get_event_id(self, event: keyboard._keyboard_event.KeyboardEvent) -> str:
-        """
-        Overrides parent method
-        """
-        return event.name
-
-
-class GamePadConfig(EventHandlerConfig):
-    """
-    Handler for a gamepad using the evdev package
-    """
-    # TODO: make a virtual gamepad handler
-    # TODO: create an interface for the touchpad
-    NULL = -1
-    TRIANGLE = 0
-    SQUARE = 1
-    CIRCLE = 2
-    CROSS = 3
-    L1 = 4
-    R1 = 5
-    L2 = 6
-    R2 = 7
-    L3 = 8
-    R3 = 9
-    CREATE = 10
-    OPTIONS = 11
-    PS = 12
-    TOUCHPAD = 13
-    LH = 14
-    LV = 15
-    RH = 16
-    RV = 17
-    DIRH = 18
-    DIRV = 19
-    
-    BUTTONS = {TRIANGLE: "triangle", SQUARE: "square", CIRCLE: "circle", CROSS: "cross", L1: "L1", R1: "R1", L3: "L3", R3: "R3", CREATE: "create", OPTIONS: "options", PS: "PS", TOUCHPAD: "touchpad"}
-    ANALOG_TRIGGERS = {RH: "RH", RV: "RV", LH: "LH", LV: "LV", L2: "L2", R2: "R2", DIRH: "dirH", DIRV: "dirV"}
-    INPUTS = BUTTONS | ANALOG_TRIGGERS
-    DEFAULT_IDS = {L1: 310, R1: 311, L2: 2, R2: 5, PS: 316, LH: 0, LV: 1, RH: 3, RV: 4, DIRH: 16, DIRV: 17, TRIANGLE: 307, SQUARE: 308, CIRCLE: 305, CROSS: 304}
-
-    def __init__(self, **kwargs):
-        self.device = self.connect()
-        kwargs |= self._get_additional_init_kwords()
-        super().__init__(**kwargs)
-        
-    def valid_event(self, event: evdev.events.InputEvent) -> bool:
-        """
-        Overrides parent method
-        """
-        # ignore event type 0 (not sure what it corresponds to but results in weird behaviour)
-        return event.type != 0
-    
-    def get_event_id(self, event: evdev.events.InputEvent) -> int:
-        """
-        Overrides parent method
-        """
-        return event.code
-    
-    def get_event_raw_value(self, event: evdev.events.InputEvent) -> float:
-        """
-        Overrides parent method
-        """
-        return event.value
-    
-    def connect(self) -> evdev.InputDevice:
-        print("connecting to gamepad...")
-        while True:
-            for device in map(evdev.InputDevice, evdev.list_devices()):
-                print(device)
-                return device
-            sleep(1)
-
-    def read_inputs(self):
-        """
-        Overrides parent method
-        """
-        try:
-            for event in self.device.read_loop():
-                self.handle_event(event)
-
-        except (TypeError, IOError) as e:
-            raise
-    
-    def _get_additional_init_kwords(self):
-        """
-        Get custom parameters corresponding to the detected device (returns empty dict if the device name is not known)
-        """
-        if self.device.name == "Generic X-Box pad":
-            return {
-                "circle_id": 305,
-                "triangle_id": 308,
-                "square_id": 307,
-                "cross_id": 304,
-                "RH_amplitude": 2**15,
-                "RV_amplitude": 2**15,
-                "LH_amplitude": 2**15,
-                "LV_amplitude": 2**15,
-                "R2_amplitude": 2**8,
-                "L2_amplitude": 2**8
-            }
-        elif self.device.name == "Wireless Controller":
-            return {
-                "circle_id": 305,
-                "triangle_id": 307,
-                "square_id": 308,
-                "cross_id": 304,
-                "RH_offset": 2**7,
-                "RV_offset": 2**7,
-                "LH_offset": 2**7,
-                "LV_offset": 2**7,
-                "RH_amplitude": 2**7,
-                "RV_amplitude": 2**7,
-                "LH_amplitude": 2**7,
-                "LV_amplitude": 2**7,
-                "R2_amplitude": 2**8,
-                "L2_amplitude": 2**8
-            }
-        return {}
-
-
-def test(val: float, label: str = "AAA"):
-    print(label, val)
-
-
-if __name__ == "__main__":
-    controller = KeyboardConfig()
-    controller.bind(KeyboardConfig._2, test, "val", label=2)
-    controller.bind(KeyboardConfig.e, test, "val", label="e")
-    controller.bind(KeyboardConfig.E, test, "val", label="E")
-    while True:
-        controller.read_inputs()
