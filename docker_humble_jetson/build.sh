4e87bfa970efb2744239282bc2aea7c173787e2f
Ugo Balducci <51507845+TheGuyWithoutH@users.noreply.github.com>
2024-08-03 13:56:44 +0000

    add build dockers and fix dependencies

--- docker_humble_jetson/build.sh
+++ docker_humble_jetson/build.sh
@@ -1,2 +1,3 @@
-docker build --pull --no-cache --progress=plain -t ghcr.io/epflxplore/hd:humble-jetson -f Dockerfile ..
+docker build --pull --no-cache --progress=plain -t ghcr.io/epflxplore/hd:humble-jetson-torch -f Dockerfile -t hd-ros-torch ..
+# docker build --pull --no-cache --progress=plain -t ghcr.io/epflxplore/hd:humble-jetson -f Dockerfile -t hd-ros ..
 # docker build --pull --progress=plain -t ghcr.io/epflxplore/hd:humble-jetson -f Dockerfile ..
