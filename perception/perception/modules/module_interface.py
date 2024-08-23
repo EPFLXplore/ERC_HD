30577414ad8cd5c30d20d0717b868e342fe771cb
Matthieu <matthieu.andre@epfl.ch>
2024-08-23 21:36:21 +0200

    draw modules

--- perception/perception/modules/module_interface.py
+++ perception/perception/modules/module_interface.py
@@ -2,7 +2,7 @@ from numpy import ndarray
 
 
 class ModuleInterface:
-    def __init__(self, confg_file):
+    def __init__(self, confg_file) -> None:
         raise NotImplementedError(
             "__init__ method should be implemented in the child class"
         )
@@ -10,4 +10,9 @@ class ModuleInterface:
     def __call__(self, rgb_frame: ndarray, depth_frame: ndarray):
         raise NotImplementedError(
             "__call__ method should be implemented in the child class"
+        )
+
+    def draw(frame: ndarray) -> None:
+        raise NotImplementedError(
+            "draw method should be implemented in the child class"
         )
