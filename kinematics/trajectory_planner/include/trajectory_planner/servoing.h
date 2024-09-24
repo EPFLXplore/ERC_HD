05663814000bd0ae2d54c490c27d69045b042301
maschull106 <matthias.schuller@epfl.ch>
2024-09-25 01:20:09 +0200

    new servoing design should be better but performs worse :o

--- kinematics/trajectory_planner/include/trajectory_planner/servoing.h
+++ kinematics/trajectory_planner/include/trajectory_planner/servoing.h
@@ -12,11 +12,46 @@
 #include "std_msgs/msg/int8.hpp"
 
 #include <geometry_msgs/msg/point.h>
+#include <geometry_msgs/msg/pose.h>
 
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 
 using std::placeholders::_1;
+
+
+struct JointGoalInfo {
+    bool                                                is_active = false;
+    std::vector<double>                                 target_state;
+    std::vector<double>                                 initial_state;
+    geometry_msgs::msg::Pose                            target_pose;    // information not needed to compute joint space trajectory but can be useful
+    std::chrono::time_point<std::chrono::steady_clock>  start_time = std::chrono::steady_clock::now();
+    double                                              execution_time_seconds;
+
+    void start() {
+        start_time = std::chrono::steady_clock::now();
+        is_active = true;
+    }
+
+    void stop() {
+        is_active = false;
+    }
+
+    void getAdvancement(std_msgs::msg::Float64MultiArray &array) {
+        auto now = std::chrono::steady_clock::now();
+        double elapsed_time = std::chrono::duration_cast<std::chrono::seconds>(now-start_time).count();
+        if (elapsed_time >= execution_time_seconds) {
+            array.data = target_state;
+            stop();
+            return;
+        }
+        double ratio = elapsed_time / execution_time_seconds;
+        array.data.clear();
+        for (uint i = 0; i < target_state.size(); i++) {
+            array.data.push_back(initial_state[i] + (target_state[i]-initial_state[i])*ratio);
+        }
+    }
+};
 
 
 class ServoPlanner : public rclcpp::Node {
@@ -46,17 +81,21 @@ private:
 
     void getEEFPose(geometry_msgs::msg::Pose &pose);
 
+    void copyCurrentJointState(std::vector<double> &destination);
+
     void modeChangeCallback(const std_msgs::msg::Int8::SharedPtr msg);
 
     void directionalTorqueCallback(const geometry_msgs::msg::Point::SharedPtr torque);
 
-    void followDirectionalTorque(const geometry_msgs::msg::Point::SharedPtr torque);
+    void followDirection(geometry_msgs::msg::Point torque);
 
 
     const std::string                                                       m_planning_group = "kerby_arm_group";
+    const uint                                                              m_joint_count = 6;
     std::shared_ptr<moveit::planning_interface::MoveGroupInterface>         m_move_group;
     std::shared_ptr<moveit::planning_interface::PlanningSceneInterface>     m_planning_scene_interface;
     const moveit::core::JointModelGroup*                                    m_joint_model_group;
+    JointGoalInfo                                                           m_joint_goal;
     rclcpp::Subscription<std_msgs::msg::Int8>::SharedPtr                    m_mode_change_sub;
     rclcpp::Subscription<geometry_msgs::msg::Point>::SharedPtr              m_direction_torque_sub;
     rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr          m_posistion_command_pub;
--- kinematics/trajectory_planner/src/servoing.cpp
+++ kinematics/trajectory_planner/src/servoing.cpp
@@ -17,6 +17,12 @@ void ServoPlanner::config()
     m_move_group = std::make_shared<moveit::planning_interface::MoveGroupInterface>(shared_from_this(), m_planning_group);
     m_planning_scene_interface = std::make_shared<moveit::planning_interface::PlanningSceneInterface>();
     m_joint_model_group = m_move_group->getCurrentState()->getJointModelGroup(m_planning_group);
+    for (size_t i=0; i < m_joint_count; i++) {
+        m_joint_goal.target_state.push_back(0.0);
+    }
+    for (size_t i=0; i < m_joint_count; i++) {
+        m_joint_goal.initial_state.push_back(0.0);
+    }
 
     createROSInterfaces();
 }
@@ -34,12 +40,33 @@ void ServoPlanner::modeChangeCallback(const std_msgs::msg::Int8::SharedPtr msg)
 }
 
 double sq(double x) { return x*x; }
+
 double squaredNorm(const geometry_msgs::msg::Point &vector) {
     return sq(vector.x) + sq(vector.y) + sq(vector.z);
 }
 
 double norm(const geometry_msgs::msg::Point &vector) {
     return std::sqrt(squaredNorm(vector));
+}
+
+double squaredDistance(const geometry_msgs::msg::Point &p1, const geometry_msgs::msg::Point &p2) {
+    return p1.x * p2.x + p1.y * p2.y + p1.z * p2.z;
+}
+
+geometry_msgs::msg::Point pointMul(const geometry_msgs::msg::Point &p, double k) {
+    geometry_msgs::msg::Point result;
+    result.x = p.x * k;
+    result.y = p.y * k;
+    result.z = p.z * k;
+    return result;
+}
+
+geometry_msgs::msg::Point pointAdd(const geometry_msgs::msg::Point &p1, const geometry_msgs::msg::Point &p2) {
+    geometry_msgs::msg::Point result;
+    result.x = p1.x + p2.x;
+    result.y = p1.y + p2.y;
+    result.z = p1.z + p2.z;
+    return result;
 }
 
 void normalize(geometry_msgs::msg::Point &vector) {
@@ -51,41 +78,57 @@ void normalize(geometry_msgs::msg::Point &vector) {
     }
 }
 
+void vectorCopy(const std::vector<double> &origin, std::vector<double> &destination) {
+    destination.clear();
+    for (auto x: origin) {
+        destination.push_back(x);
+    }
+}
+
+void quaternionCopy(const geometry_msgs::msg::Quaternion &origin, geometry_msgs::msg::Quaternion &destination) {
+    destination.x = origin.x;
+    destination.y = origin.y;
+    destination.z = origin.z;
+    destination.w = origin.w;
+}
+
 void ServoPlanner::directionalTorqueCallback(const geometry_msgs::msg::Point::SharedPtr torque) {
-    std::thread executor(&ServoPlanner::followDirectionalTorque, this, torque);
+    static const double treshold_torque = 0.5;
+    if (squaredNorm(*torque) < sq(treshold_torque)) return;
+
+    std::thread executor(&ServoPlanner::followDirection, this, *torque);
     executor.detach();
 }
 
-void ServoPlanner::followDirectionalTorque(const geometry_msgs::msg::Point::SharedPtr torque)
+void ServoPlanner::followDirection(geometry_msgs::msg::Point direction)
 {
     // TODO:
     // increase frequency
     // maybe interpolate between distinct commands
     // maybe (probably) update command at lower rate than sending
-    double treshold_torque = 0.5;
-    if (squaredNorm(*torque) < sq(treshold_torque)) return
 
-    normalize(*torque);
+    static const double travel_distance = 0.03;  // [m]
     geometry_msgs::msg::Pose current_pose;
     getEEFPose(current_pose);
-    geometry_msgs::msg::Point current_point = current_pose.position;
-    geometry_msgs::msg::Point target_point;
-    double scaling = 0.005;  // [m]
-    target_point.x = current_point.x + torque->x * scaling;
-    target_point.y = current_point.y + torque->y * scaling;
-    target_point.z = current_point.z + torque->z * scaling;
-    geometry_msgs::msg::Pose target_pose;
-    target_pose.position = target_point;
-    target_pose.orientation = current_pose.orientation;
-    std::vector<double> target_joint_config;
-    if (getIK(target_pose, target_joint_config)) {
-        // send target joint positions to motor control
-        std_msgs::msg::Float64MultiArray msg;
-        for (auto x: target_joint_config) {
-            msg.data.push_back(x);
-        }
-        m_posistion_command_pub->publish(msg);
+    double new_goal_treshold_distance = travel_distance * 0.2;
+    if (m_joint_goal.is_active && squaredDistance(current_pose.position, m_joint_goal.target_pose.position) > sq(new_goal_treshold_distance)) {
+        return;
     }
+
+    normalize(direction);
+    geometry_msgs::msg::Point target_point = pointAdd(current_pose.position, pointMul(direction, travel_distance));
+    m_joint_goal.target_pose.position = target_point;
+    quaternionCopy(current_pose.orientation, m_joint_goal.target_pose.orientation);
+    m_joint_goal.target_pose.orientation = current_pose.orientation;
+
+    m_joint_goal.target_state.clear();
+    if (!getIK(m_joint_goal.target_pose, m_joint_goal.target_state)) {
+        return;
+    }
+    copyCurrentJointState(m_joint_goal.initial_state);
+    static const double execution_speed = 0.05;      // [m/s] in cartesian space
+    m_joint_goal.execution_time_seconds = travel_distance * execution_speed;
+    m_joint_goal.start();
 }
 
 void ServoPlanner::getEEFPose(geometry_msgs::msg::Pose &pose) {
@@ -100,13 +143,17 @@ bool ServoPlanner::getIK(const geometry_msgs::msg::Pose &ik_pose, const std::vec
 bool ServoPlanner::getIK(const geometry_msgs::msg::Pose &ik_pose, std::vector<double> &solution) {
     const double *positions = m_move_group->getCurrentState()->getVariablePositions();
     std::vector<double> current_state_positions;
-    static size_t MOTOR_COUNT = 6;
-    for (size_t i = 0; i < MOTOR_COUNT; i++) {
-        current_state_positions.push_back(positions[i]);
-    }
+    copyCurrentJointState(current_state_positions);
     return getIK(ik_pose, current_state_positions, solution);
 }
 
+void ServoPlanner::copyCurrentJointState(std::vector<double> &destination) {
+    const double *positions = m_move_group->getCurrentState()->getVariablePositions();
+    destination.clear();
+    for (size_t i = 0; i < m_joint_count; i++) {
+        destination.push_back(positions[i]);
+    }
+}
 
 
 void ServoPlanner::spin()
@@ -121,7 +168,12 @@ void ServoPlanner::loop()
     {
         switch(m_mode) {
             case ServoPlanner::CommandMode::COMPLIANT_MOTION:
-                RCLCPP_INFO(this->get_logger(), "COMPLIANT BEHAVIOUR MODE");
+                // RCLCPP_INFO(this->get_logger(), "COMPLIANT BEHAVIOUR MODE");
+                if (m_joint_goal.is_active) {
+                    std_msgs::msg::Float64MultiArray msg;
+                    m_joint_goal.getAdvancement(msg);
+                    m_posistion_command_pub->publish(msg);
+                }
                 break;
         }
         rate.sleep();
