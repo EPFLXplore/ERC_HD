2a05866073192492edb2a6a5e47d46fad5e49f5e
maschull106 <matthias.schuller@epfl.ch>
2024-10-03 17:00:09 +0200

    new fake torque sensor script for gamepad

--- kinematics/fake_components/scripts/fake_torque_sensor_gamepad.py
+++ kinematics/fake_components/scripts/fake_torque_sensor_gamepad.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from kinematics_utils.node_template import NodeTemplate
-from input_handling.keyboard import KeyboardConfig
+from input_handling.gamepad import GamePadConfig
 from kinematics_utils.quaternion_arithmetic_new import Point
 import geometry_msgs.msg as gmsg
 import kinematics_utils.pose_tracker as pt
@@ -12,11 +12,12 @@ class FakeTorqueSensor(NodeTemplate):
     def __init__(self):
         super().__init__("fake_bota_sensor")
         self.torque = Point()
-        self.input_config = KeyboardConfig()
-        self.input_config.bind(KeyboardConfig.left, self.torque.set_x, "x")
-        self.input_config.bind(KeyboardConfig.right, lambda x: self.torque.set_x(-x), "x")
-        self.input_config.bind(KeyboardConfig.up, self.torque.set_z, "z")
-        self.input_config.bind(KeyboardConfig.down, lambda z: self.torque.set_z(-z), "z")
+        self.input_config = GamePadConfig()
+        self.input_config.bind(GamePadConfig.RH, self.torque.set_x, "x")
+        self.input_config.bind(GamePadConfig.RV, lambda y: self.torque.set_y(-y), "y")
+        self.input_config.bind(GamePadConfig.R2, self.torque.set_z, "z")
+        self.input_config.bind(GamePadConfig.L2, lambda z: self.torque.set_z(-z), "z")
+        self.input_config.background_loop()
         
         self.in_eef_frame = True
 
@@ -27,8 +28,9 @@ class FakeTorqueSensor(NodeTemplate):
         torque = self.torque
         if self.in_eef_frame:
             torque = pc.gripper_to_abs_vector(torque)
+        print(torque)
         self.torque_pub.publish(torque.publishable())
 
 
 if __name__ == "__main__":
-    FakeTorqueSensor.main()
+    FakeTorqueSensor.main(hz=30)
